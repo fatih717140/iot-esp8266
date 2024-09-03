73d065d6b78ede5ab18ac6368ad88344c42ba83d
fatih ceylan <fatih_ceylan4072@outlook.com>
2024-09-03 12:39:33 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+#imported flask and render_teplate 
 from flask import Flask,render_template
-
+#note:To use a not render template, create a file in the main.py directory.   
+#Create a temlates folder and move the index.html file into it
 
 app=Flask(__name__)
 
